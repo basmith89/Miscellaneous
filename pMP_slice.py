c847c604a52a6f4ac8855615b970ba5e77beda2d
basmith89 <basmith@email.arizona.edu>
2017-05-23 19:24:25 -0700

    Updated slicer script with more options

--- pMP_slice.py
+++ pMP_slice.py
@@ -21,6 +21,10 @@ parser.add_argument("-s", "--start_slice", required = False,
                     type = int, help = "Select a starting position to cut [START:]")
 parser.add_argument("-e", "--end_slice", required = False,
                     type = int, help = "Select an ending position to cut [:END]")
+parser.add_argument("-R1", "--range1", required = False,
+                    type = int, help = "Start of range slice extract [START:end]")
+parser.add_argument("-R2", "--range2", required = False,
+                    type = int, help = "End of range slice extract [start:END]")
 
 args = parser.parse_args()
 
@@ -30,6 +34,9 @@ out_file = open(args.output, 'w')
 if args.start_slice:
     start_slice = args.start_slice - 1
     print start_slice
+
+if args.range1:
+    range1 = args.range1 - 1
 
 with open(args.input, 'r') as f:
     for line in f:
@@ -47,4 +54,8 @@ with open(args.input, 'r') as f:
                 out_file.write(line[start_slice:])
             elif args.end_slice:
                 out_file.write(line[:args.end_slice])
+            elif args.range1 and args.range2:
+                out_file.write(line[range1:args.range2])
+            else:
+                print "Improper usage.  Please use -h for help."
             out_file.write("\n")
